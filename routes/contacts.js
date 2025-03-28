296b491c45219ac34754f9fe8de3fa606328c291
gustavolimadesousa <gustavolimadesousa@hotmail.com>
2025-03-28 00:06:21 +0000

    install-swagger

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,6 +7,13 @@ router.get('/', usersController.getAll);
 
 router.get('/:id', usersController.getSingle);
 
+router.post("/", usersController.createContact);
+
+router.put("/:id", usersController.updateContact);
+
+router.delete("/:id", usersController.deleteContact);
+
+
 module.exports =
   router;
 
